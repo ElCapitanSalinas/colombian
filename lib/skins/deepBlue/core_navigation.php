383b7cf7335cf954ebcaf127c0305b33e8f98019
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2020-10-23 17:50:18 -0500

    Update core_navigation.php

--- lib/skins/deepBlue/core_navigation.php
+++ lib/skins/deepBlue/core_navigation.php
@@ -28,6 +28,7 @@
         <i class="fa fa-briefcase"></i> Nosotros
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+         <a class="dropdown-item" href="https://colombianairways.com/index.php/pages/objetivos">Objetivos</a>
           <a class="dropdown-item" href="<?php echo SITE_URL ?>/index.php/Pilots">Pilotos</a>
           <a class="dropdown-item" href="<?php echo SITE_URL ?>/index.php/vStaff">Staff</a>
           <div class="dropdown-divider"></div>
