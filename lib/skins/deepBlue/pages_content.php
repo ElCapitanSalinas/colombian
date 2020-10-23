ed8d5f00408083a1450954c5d36ae451be79a212
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2020-10-23 16:49:34 -0500

    paginas

--- lib/skins/deepBlue/pages_content.php
+++ lib/skins/deepBlue/pages_content.php
@@ -1,7 +1,8 @@
 <section class="page-contents">
 <div class="container">
 <?php if(!defined('IN_PHPVMS') && IN_PHPVMS !== true) { die(); } ?>
-<h1><i class="fa fa-plane" fa-lg"></i> <?php echo $pagename;?></h1>
+<br>
+<h1><b><i class="fa fa-plane" fa-lg"></i> <?php echo $pagename;?></b></h1>
 <div>
 <?php echo html_entity_decode($content);?>
 </div>
