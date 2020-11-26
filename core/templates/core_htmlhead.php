b82476143a3f639d43bfa64393d91e100651231d
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2020-11-26 12:40:56 -0500

    Update core_htmlhead.php

--- core/templates/core_htmlhead.php
+++ core/templates/core_htmlhead.php
@@ -35,7 +35,7 @@ var baseurl = "<?php echo SITE_URL;?>";
 <!--
  * Add Google Maps API key to next line. https://developers.google.com/maps/documentation/javascript/get-api-key 
  -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY_HERE"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGo1W8rA2v4SCtxx3Ei5_GkVJMUkHoUQk"></script>
 <script type="text/javascript" src="<?php echo fileurl('lib/js/jquery.form.js');?>"></script>
 <script type="text/javascript" src="<?php echo fileurl('lib/js/phpvms.js');?>"></script>
 
