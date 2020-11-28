0eb37bc73565a17f5a18dd9f31f47facf27ab953
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2020-11-27 23:44:12 -0500

    Update downloads_list.php

--- lib/skins/deepBlue/downloads_list.php
+++ lib/skins/deepBlue/downloads_list.php
@@ -7,9 +7,9 @@
 <div class="container">
 <br />
 <div class="row">
-  <div class="col-lg-3">
-        <div class="card w-175">
-		<?php 
+	<div class="col-lg-3">
+		<div class="card w-175">
+			<?php 
 		if(!$allcategories)
 		{
 			echo 'No hay descargas disponibles!';
@@ -19,11 +19,12 @@
 		foreach($allcategories as $category)
 		{
 		?>
-		<p><h2><strong></strong></h2></p>
-		<h5 class="card-header" style="background-color: #0A1437;"><b><font color="#FFFFFF"><?php echo $category->name?></font></b></h5>
-		<ul>
+			<p>
+				<h2><strong></strong></h2></p>
+			<h5 class="card-header" style="background-color: #0A1437;"><b><font color="#FFFFFF"><?php echo $category->name?></font></b></h5>
+			<ul>
 
-		<?php	
+				<?php	
 			# This loops through every download available in the category
 			$alldownloads = DownloadData::GetDownloads($category->id);
 			
@@ -36,19 +37,24 @@
 			foreach($alldownloads as $download)
 			{
 		?>
-        <div class="card-body">
-		<img class="img-fluid" src="<?php echo $download->image; ?>" alt="<?php echo $download->name; ?>">
-		<br>	
-		Nombre:<a href="<?php echo url('/downloads/dl/'.$download->id);?>">
-			 <?php echo $download->name?></a><br />
-	      Descripción: <?php echo $download->description?><br />
-          <em>Descargado <?php echo $download->hits?> veces</em></li>
-			<?php
+					<div class="card-body">
+						<img class="img-fluid" src="<?php echo $download->image; ?>" alt="<?php echo $download->name; ?>">
+						<br> Nombre:
+						<a href="<?php echo url('/downloads/dl/'.$download->id);?>">
+							<?php echo $download->name?>
+						</a><br /> Descripción:
+						<?php echo $download->description?><br />
+						<em>Descargado <?php echo $download->hits?> veces</em></li>
+						<?php
 				}
-			?><hr />
-				<?php
+			?>
+							<hr />
+							<?php
 			}
 			?>
 
+					</div>
 		</div>
+	</div>
+</div>
 </section>
