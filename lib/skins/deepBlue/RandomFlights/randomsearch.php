2d50b6d05dfa3f00818755892a7678a12ac4898c
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2020-12-31 12:14:10 -0500

    Update randomsearch.php

--- lib/skins/deepBlue/RandomFlights/randomsearch.php
+++ lib/skins/deepBlue/RandomFlights/randomsearch.php
@@ -17,7 +17,43 @@ $airlines = OperationsData::getAllAirlines(true);
 
 ?>
 
-
+<section class="page-contents">
+<div class="container">
+<br />
+<div class="row">
+  <div class="col-sm-1">
+  </div>
+  <div class="col-lg-10">
+        <div class="card w-175">
+        <h5 class="card-header" style="background-color: #0A1437;"><b><font color="#FFFFFF"><i class="fa fa-star" fa-lg style="color:#FFFFFF"></i> Medallas</font></b></h5>
+        <div class="card-body">
+        <table class="table table-bordered">
+			<thead>
+				<tr>
+				<th scope="col">Premio</th>
+				<th scope="col">Descripción</th>
+				<th scope="col">Imagen</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php
+			foreach($awards as $aw);
+			{
+			?>
+			<tr id="row<?php echo $aw->awardid;?>">
+				<td align="center"><?php echo $aw->name; ?></td>
+				<td align="center"><?php echo $aw->descrip; ?></td>
+				<td align="center"><img src="<?php echo $aw->image; ?>" /></td>
+				</tr>
+			<?php
+			}
+			?>
+	</tbody>
+	</table>
+	</div>
+	</div>
+	</div>
+</section>
 
 <table border = "0">
     <tr>
