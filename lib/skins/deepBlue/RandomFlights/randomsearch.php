4f5d4ae14ae6e60a64de4595a2faf272a876bb6c
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2020-12-31 12:32:32 -0500

    Update randomsearch.php

--- lib/skins/deepBlue/RandomFlights/randomsearch.php
+++ lib/skins/deepBlue/RandomFlights/randomsearch.php
@@ -66,9 +66,9 @@ $airlines = OperationsData::getAllAirlines(true);
             <tr>
                 <td width ="25%"><b>Número de vuelos a agendar:</b></td>
                 <td width ="75%"><select id="count" name="count" class="form-control">
-                    <option value="5">5</option>
+                    <option value="6">6</option>
                     <option value="10">10</option>
-                    <option value="15">15</option>
+                    <option value="16">16</option>
                 </select></td>
             </tr>
             <tr>
