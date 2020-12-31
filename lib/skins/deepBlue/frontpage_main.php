33199827555a1edea6e2fd3f60fa6bcf3488ebbb
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2020-12-31 18:54:34 -0500

    Update frontpage_main.php

--- lib/skins/deepBlue/frontpage_main.php
+++ lib/skins/deepBlue/frontpage_main.php
@@ -23,6 +23,7 @@
 
 <div class="row">
   <div class="col-lg-2">
+  </div>
   <div class="col-lg-10">
   <div class="card w-175">
         <h5 class="card-header" style="background-color: #0A1437;"><b><font color="#FFFFFF"><i class="fa fa-star" fa-lg style="color:#FFFFFF"></i> Top aterrizajes</font></b></h5>
@@ -50,31 +51,8 @@
             }
         ?>
         </table>
-        <table class="table table-bordered">
-			<thead>
-				<tr>
-				<th scope="col">Premio</th>
-				<th scope="col">Descripción</th>
-				<th scope="col">Imagen</th>
-				</tr>
-			</thead>
-			<tbody>
-			<?php
-			foreach($awards as $aw);
-			{
-			?>
-			<tr id="row<?php echo $aw->awardid;?>">
-				<td align="center"><?php echo $aw->name; ?></td>
-				<td align="center"><?php echo $aw->descrip; ?></td>
-				<td align="center"><img src="<?php echo $aw->image; ?>" /></td>
-				</tr>
-			<?php
-			}
-			?>
-	</tbody>
-	</table>
-	</div>
-	</div>
+	    </div>
+	  </div>
   </div>
 </div>
 
