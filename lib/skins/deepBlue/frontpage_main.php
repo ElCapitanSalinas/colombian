56ffa11a8cb6fa476589fcc4eacf52228afeee55
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2021-01-03 17:36:57 -0500

    Update frontpage_main.php

--- lib/skins/deepBlue/frontpage_main.php
+++ lib/skins/deepBlue/frontpage_main.php
@@ -37,22 +37,6 @@
 </div>
 <br>
 <br>
-<section class="page-contents">
-<div class="container">
-<br />
-<div class="row">
-  <div class="col-sm-1">
-  </div>
-  <div class="col-lg-10">
-        <div class="card w-175">
-        <h5 class="card-header" style="background-color: #0A1437;"><b><font color="#FFFFFF"><i class="fa fa-star" fa-lg style="color:#FFFFFF"></i> Medallas</font></b></h5>
-        <div class="card-body">
-        <?php Screenshots::show_newest_screenshot(); ?>
-	      </div>
-	      </div>
-	</div>
-</section>
-<br>
 <div class="row">
   <div class="col-lg-9">
         <div class="card w-175">
