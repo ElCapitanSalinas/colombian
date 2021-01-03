d2729b5a214c772000d5910e7389e927d551da1f
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2021-01-03 15:41:35 -0500

    Update screenshots_form.php

--- lib/skins/deepBlue/Screenshots/screenshots_form.php
+++ lib/skins/deepBlue/Screenshots/screenshots_form.php
@@ -9,44 +9,59 @@
 //@copyright Copyright (c) 2009-2010, David Clark
 //@license http://creativecommons.org/licenses/by-nc-sa/3.0/
 ?>
+<section class="page-contents">
+<div class="container">
+<br />
+<div class="row">
+  <div class="col-sm-1">
+  </div>
+  <div class="col-lg-10">
+        <div class="card w-175">
+        <h5 class="card-header" style="background-color: #0A1437;"><b><font color="#FFFFFF"><i class="fa fa-star" fa-lg style="color:#FFFFFF"></i> Medallas</font></b></h5>
+        <div class="card-body">
+                    
+            <form action="<?php echo url('/Screenshots');?>" method="post" enctype="multipart/form-data">
+                <table class="profiletop">
+                    <tr>
+                        <td width="50%" valign="top">
+                            <h5>Para tener en cuenta:</h5>
+                            <ul>
+                                <li>No está permitido subir imagenes no relacionadas con la aerolinea</li>
+                                <li>Está prohibido subir contenido pornografico o de cualquier otra indole</li>
+                                <li>En lo posible subir imagenes relacionadas con el simulador</li>
+                            </ul>
+                        </td>
+                        <td>
+                            <p>
+                                <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max_file_size ?>">
+                            </p>
 
-<form action="<?php echo url('/Screenshots');?>" method="post" enctype="multipart/form-data">
-    <table class="profiletop">
-        <tr>
-            <td width="50%" valign="top">
-                <h5>Put Your Screenshot upload terms:</h5>
-                <ul>
-                    <li>Rule 1.</li>
-                    <li>Rule 2.</li>
-                </ul>
-            </td>
-            <td>
-                <p>
-                    <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max_file_size ?>">
-                </p>
+                            <p>
+                                <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
+                                <label for="file">Subir:</label><br />
+                                <input class="mail" id="file" type="file" name="uploadedfile" />
+                            </p>
 
-                <p>
-                    <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
-                    <label for="file">File to upload:</label><br />
-                    <input class="mail" id="file" type="file" name="uploadedfile" />
-                </p>
+                            <p>
+                                <label for="description">Ingresa una descripción para la imagen:</label><br />
+                                <textarea class="mail" name="description" class="form-control" rows="5" cols="50"></textarea>
+                            </p>
 
-                <p>
-                    <label for="description">Enter a description for your screenshot:</label><br />
-                    <textarea class="mail" name="description" rows="5" cols="50"></textarea>
-                </p>
+                            <p>
+                                <input type="hidden" name="action" value="save_upload" />
+                                <input class="mail" type="submit" value="Subir archivo!">
+                            </p>
+                        </td>
+                    </tr>
+                </table>
+            </form>
 
-                <p>
-                    <input type="hidden" name="action" value="save_upload" />
-                    <input class="mail" type="submit" value="Upload File!">
-                </p>
-            </td>
-        </tr>
-    </table>
-</form>
-
-<center>
-    <br />
-    <form method="link" action="<?php echo SITE_URL; ?>/index.php/screenshots">
-        <input class="mail" type="submit" value="Return To The Gallery"></form>
-</center>       
+            <center>
+                <br />
+                <form method="link" action="<?php echo SITE_URL; ?>/index.php/screenshots">
+                    <input class="mail" type="submit" value="Volver a galería"></form>
+            </center>       
+	</div>
+	</div>
+	</div>
+</section>
