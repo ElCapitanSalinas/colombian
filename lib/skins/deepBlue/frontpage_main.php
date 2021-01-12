a2e9f6d36151e904e3132d3f223f8c44a5b17009
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2021-01-12 18:22:37 -0500

    Update frontpage_main.php

--- lib/skins/deepBlue/frontpage_main.php
+++ lib/skins/deepBlue/frontpage_main.php
@@ -6,11 +6,11 @@
     </script>
 </head>
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Buenas noticias!</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
