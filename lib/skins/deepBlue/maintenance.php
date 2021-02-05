a2816737f5f4ee64058a1245f45fac1f8358e5c3
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2021-02-05 18:37:04 -0500

    Actualización Mantenimiento

--- lib/skins/deepBlue/maintenance.php
+++ lib/skins/deepBlue/maintenance.php
@@ -1,24 +1,22 @@
-<section class="page-contents">
-<div class="container">
 <?php if(!defined('IN_PHPVMS') && IN_PHPVMS !== true) { die(); } ?>
 <?php
     /* This page should be a complete HTML page */
 ?>
 <html>
 <head>
-<title>Down for maintenance - phpVMS Virtual Airline</title>
+<title>Sistema caido. - Colombian Airways.</title>
 <style type="text/css">
 body { font-family: verdana;  font-size: 16px; color: #001B6B }
 h1 { font-weight:16px;font-size:18px;color:#6B001B;margin:0 0 4px 0; }
 div#codon_crit_error { border:#999 1px solid;background-color:#fff;padding:20px 20px 12px 20px;}
 </style>
 </head>
-<body>		
-    <div id="codon_crit_error" >
-    <h3><i class="fa fa-cogs" fa-lg"></i> Down for maintenance</h3>
-        <p>We are currently down for maintenance, please check back soon.</p>
+<body>
+    <div class="alert alert-danger" role="alert">
+    <h4 class="alert-heading">El sistema se encuentra caido</h4>
+    <p>Parece que una clase de hpta está haciendo algo con el servidor.</p>
+    <hr>
+    <p class="mb-0">Contacte con el webmaster.</p>
     </div>
 </body>
 </html>
-</div>
-</section>
